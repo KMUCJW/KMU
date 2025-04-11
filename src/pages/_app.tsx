03d74e1d860282177c26c9c947c6ee680120e5cb
최정우 <choejeong-u@choejeong-uui-MacBookPro.local>
2025-04-11 12:14:00 +0900

    Fix global styles for dark theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,8 +11,8 @@ const GlobalStyle = createGlobalStyle`
   body {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
       Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    background: #ffffff;
-    color: #000000;
+    background: #000000;
+    color: #ffffff;
   }
 
   button {
